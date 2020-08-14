ad34320d0829819868517700bd8fff782418c1c4
DH <duane9@gmail.com>
2020-08-14 14:37:21 +0000

    Edited __init__.py

--- printbreakpoint/__init__.py
+++ printbreakpoint/__init__.py
@@ -11,5 +11,5 @@ def pb(*args):
     line = info.lineno
     fn = info.function
     divider = '|' if args else ''
-    meta = f'\033[92mpb\033[0m | {filename}:{line} in {fn}() {divider}'
+    meta = f'\033[92mpb\033[0m | {filename}:{line} in {fn} {divider}'
     print(meta, *args, file=sys.stderr)
